736705f1314d88e4a7470b6351721a2104039685
XS3B4X <sebastian2003.saez@gmail.com>
2026-06-25 15:40:33 -0400

    feat(api): Agrega endpoints de inscribir en ruta
    Se agrega el endpoint que permite a un voluntario inscribirse en una ruta

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { DeleteEnrolledRoutesService } from './volunteer/enroll/delete_enrolled_routes.service';
+import { EnrollNewRoutesController } from './volunteer/enroll/enroll_new_routes.controller';
+import { EnrollNewRoutesService } from './volunteer/enroll/enroll_new_routes.service';
+import { DeleteEnrolledRoutesController } from './volunteer/enroll/delete_enrolled_routes.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), PrismaModule],
+  controllers: [AppController, DeleteEnrolledRoutesController, EnrollNewRoutesController],
+  providers: [AppService, DeleteEnrolledRoutesService, EnrollNewRoutesService],
 })
 export class AppModule {}
