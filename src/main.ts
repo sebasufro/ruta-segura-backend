82726c236c9019c2196fde1b8f68876ccd3ed3af
Yhiazer <b.urrea01@ufromail.cl>
2026-07-03 14:19:13 -0400

    config(api): habilitar cors para cliente web
    Se configuró el middleware de CORS en el servidor para permitir peticiones HTTP desde el frontend.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000, '0.0.0.0');
 }
